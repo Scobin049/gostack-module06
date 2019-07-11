2a88417ce29ad6b09a9e28c6f1cd9a01cd49493c
Vitor Scobin <vhscobin@gmail.com>
2019-07-11 00:08:55 -0300

    Commit initial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return <View />;
+  }
+}
