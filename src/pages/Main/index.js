610c7eef6be7984637368a666870b0166dd91f35
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-07-23 00:41:56 -0300

    Update final

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import PropTypes from 'prop-types';
+
 import {
   Container,
   Form,
@@ -18,13 +23,41 @@ import {
 import api from '../../services/api';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -37,13 +70,18 @@ export default class Main extends Component {
       avatar: data.avatar_url,
     };
 
-    this.setState({ users: [...users, user], newUser: '' });
-
     Keyboard.dismiss();
+    this.setState({ users: [...users, user], newUser: '', loading: false });
+  };
+
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -56,8 +94,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton load={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
 
@@ -70,7 +112,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -80,6 +122,3 @@ export default class Main extends Component {
     );
   }
 }
-Main.navigationOptions = {
-  title: 'Usuários',
-};
--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,18 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Main from './pages/Main';
 import User from './pages/User';
 
-const pages = { Main, User };
-
 const Routes = createAppContainer(
-  createStackNavigator(pages, {
-    headerLayoutPreset: 'center',
-    headerBackTitleVisible: false,
-    defaultNavigationOptions: {
-      headerStyle: { backgroundColor: '#7159c1' },
-      headerTintColor: '#fff',
-    },
-  })
+  createStackNavigator(
+    { Main, User },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: { backgroundColor: '#7159c1' },
+        headerTintColor: '#fff',
+      },
+    }
+  )
 );
 
 export default Routes;
